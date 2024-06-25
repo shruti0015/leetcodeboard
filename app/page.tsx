baff5595fdaf9bb67721f8f525a51f74ed038d6b
Shrutikirti Singh <shrutikirtisingh4@gmail.com>
2024-06-26 00:21:02 +0530

    Added shadcn/ui buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -78,6 +78,12 @@ export default function Home() {
                 Perfect for learning how the framework works, prototyping a new
                 idea, or creating a demo to share online.
               </p>
+              <Button
+                className="bg-green-500 hover:bg-green-700"
+                variant="secondary"
+              >
+                Hello
+              </Button>
             </div>
             <div className="pt-8 text-base font-semibold leading-7">
               <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
