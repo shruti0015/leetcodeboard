9aa1bd52ab6929d58f879972b21707daee831da5
Shrutikirti Singh <shrutikirtisingh4@gmail.com>
2024-07-06 02:04:04 +0530

    Added site footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import { SiteHeader } from "@/components/site-header"
+import { SiteFooter } from "@/components/site-footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           <SiteHeader />
           {children}
+          <SiteFooter />
         </ThemeProvider>
       </body>
     </html>
---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "LC Board",
+  url: "https://leetcodeboard-shrutikirti-singhs-projects.vercel.app/",
+  description: "Virtual whiteboard foe leetcode problems.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/shrutikirti-singh/",
+    github: "https://github.com/shruti0015",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
