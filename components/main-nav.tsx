be84bbc3ce9223f92d6b3287c145b03e910653c7
Shrutikirti Singh <shrutikirtisingh4@gmail.com>
2024-08-24 05:57:22 +0530

    logo added

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -6,16 +6,17 @@ import { usePathname } from "next/navigation"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
+import Image from "next/image"
+import whiteboard from "./assets/whiteboard.jpg"
 
 export function MainNav() {
   const pathname = usePathname()
 
   return (
     <div className="mr-4 hidden md:flex">
+      <Image src={whiteboard} alt="" className="h-12 w-12 rounded-lg" />
       <Link href="/" className="mr-6 flex items-center space-x-2">
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
+        <span className="hidden font-bold sm:inline-block">LC Board</span>
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
         <Link
